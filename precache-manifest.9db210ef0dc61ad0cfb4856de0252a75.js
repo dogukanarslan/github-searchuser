f168dd173580694c888acd70ac8bc7ae41dcfabc
Doğukan Arslan <dogukan2arslan@hotmail.com>
2019-12-09 18:24:02 +0300

    Updates

---
+++ precache-manifest.9db210ef0dc61ad0cfb4856de0252a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93be093cdaa4e89d337fff37c3dea153",
+    "url": "/github-searchuser/index.html"
+  },
+  {
+    "revision": "d98045a1e778db1e8155",
+    "url": "/github-searchuser/static/css/main.f79ecc6c.chunk.css"
+  },
+  {
+    "revision": "71d28bc67f7689cdcd6b",
+    "url": "/github-searchuser/static/js/2.2aceee8b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/github-searchuser/static/js/2.2aceee8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d98045a1e778db1e8155",
+    "url": "/github-searchuser/static/js/main.456b2613.chunk.js"
+  },
+  {
+    "revision": "f8b798d3ed5599bbe5b5",
+    "url": "/github-searchuser/static/js/runtime-main.53a263e5.js"
+  }
+]);
