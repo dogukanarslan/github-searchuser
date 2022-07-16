23a5b35d6d38f1f2ffe68a15c865c6888f10dd89
Dogukan Arslan <dogukan2arslan@hotmail.com>
2022-07-16 14:32:23 +0300

    Add tab content

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,18 +1,61 @@
+import { useState } from 'react'
 import { Filters } from './Filters'
 import { Users } from '../../components/Users'
 import { useSelector } from 'react-redux'
+import { Nav, NavLink, NavItem, TabContent, TabPane } from 'reactstrap'
 
 export const Search = () => {
     const { data, status } = useSelector((state) => state.search)
 
+    const [activeTab, setActiveTab] = useState('users')
+
+    const toggle = (tab) => {
+        if (activeTab !== tab) {
+            setActiveTab(tab)
+        }
+    }
+
     return (
         <>
-            <Filters />
-            <Users
-                users={data?.items}
-                count={data?.total_count}
-                status={status}
-            />
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        onClick={() => toggle('users')}
+                        active={activeTab === 'users'}
+                    >
+                        Users
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        onClick={() => toggle('repositories')}
+                        active={activeTab === 'repositories'}
+                    >
+                        Repositories
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        onClick={() => toggle('commits')}
+                        active={activeTab === 'commits'}
+                    >
+                        Commits
+                    </NavLink>
+                </NavItem>
+            </Nav>
+
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="users">
+                    <Filters />
+                    <Users
+                        users={data?.items}
+                        count={data?.total_count}
+                        status={status}
+                    />
+                </TabPane>
+                <TabPane tabId="repositories">Tab2</TabPane>
+                <TabPane tabId="commits">Tab3</TabPane>
+            </TabContent>
         </>
     )
 }
