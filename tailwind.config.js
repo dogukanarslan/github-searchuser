f911bd72765c2e66bd56c5e0036ea487f019ba0f
Dogukan Arslan <dogukan2arslan@hotmail.com>
2023-09-10 12:18:56 +0300

    Add Quicksand

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Quicksand'],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/forms')],
