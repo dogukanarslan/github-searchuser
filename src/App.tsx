e85609508f9e5dd1c751e7d4974018799ff41335
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-01-19 20:10:45 +0300

    Add container padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Main, Header } from 'components';
 
 export const App = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-4">
       <Header />
       <Main />
     </div>
