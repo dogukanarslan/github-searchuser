c663f1c768e7a556027c50116d472430bd902e29
Dogukan Arslan <dogukan2arslan@hotmail.com>
2025-09-13 15:08:11 +0300

    Use card for user

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import { Endpoints } from '@octokit/types';
+import { Card } from './Card';
 
 export interface UserProps {
   user: Endpoints['GET /users']['response']['data'][0];
@@ -12,48 +13,50 @@ export const User = (props: UserProps) => {
   } = props;
 
   return (
-    <div className="block rounded-xl border p-4">
-      <div className="flex items-center gap-4">
-        <img
-          src={avatar_url}
-          className="h-16 w-16 rounded-full object-cover"
-          alt="User image"
-        />
+    <Card
+      body={
+        <div className="flex items-center gap-4">
+          <img
+            src={avatar_url}
+            className="h-16 w-16 rounded-full object-cover"
+            alt="User image"
+          />
 
-        <div>
-          <Link href={`/users/${login}`}>
-            <h3 className="text-lg font-bold">{login}</h3>
-          </Link>
+          <div>
+            <Link href={`/users/${login}`}>
+              <h3 className="text-lg font-bold">{login}</h3>
+            </Link>
 
-          <div className="flow-root">
-            <ul className="-m-1 flex flex-wrap">
-              <li className="p-1 leading-none">
-                <Link
-                  href={{
-                    pathname: `/details/${login}`,
-                    search: '?tab=followers',
-                  }}
-                  className="text-xs font-medium"
-                >
-                  Followers
-                </Link>
-              </li>
+            <div className="flow-root">
+              <ul className="-m-1 flex flex-wrap">
+                <li className="p-1 leading-none">
+                  <Link
+                    href={{
+                      pathname: `/details/${login}`,
+                      search: '?tab=followers',
+                    }}
+                    className="text-xs font-medium"
+                  >
+                    Followers
+                  </Link>
+                </li>
 
-              <li className="p-1 leading-none">
-                <Link
-                  href={{
-                    pathname: `/details/${login}`,
-                    search: '?tab=following',
-                  }}
-                  className="text-xs font-medium"
-                >
-                  Following
-                </Link>
-              </li>
-            </ul>
+                <li className="p-1 leading-none">
+                  <Link
+                    href={{
+                      pathname: `/details/${login}`,
+                      search: '?tab=following',
+                    }}
+                    className="text-xs font-medium"
+                  >
+                    Following
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    />
   );
 };
